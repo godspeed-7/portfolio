1c86997d20cbd8ffb3c53a50d51e7d3398d92061
Amnok <ankitsingh80741@gmail.com>
2021-01-12 02:03:04 +0530

    added projects images

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './projects.module.css';
+import { projects } from '../../resources/projects';
 
 export default function Projects() {
+  console.log(projects);
   return <div>This is project baby</div>;
 }
