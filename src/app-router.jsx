e3ff4bac38d715e1bbf492c73bf6f43c1c755641
Amnok <ankitsingh80741@gmail.com>
2021-01-16 22:09:57 +0530

    fixed lint

--- src/app-router.jsx
+++ src/app-router.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Route, Switch, Router } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './components/home/home';
 import Contact from './components/contact/contact';
 import Projects from './components/projects/projects';
 import ToolBar from './layout/toolbar/toolbar';
-import Footer from './layout/footer/footer';
 
 export default function AppRouter() {
   return (
